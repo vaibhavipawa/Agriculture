6e3b39cdfa75fee4b1531c54b5fe5f6b594b6edc
Agriculture <vaibhavipawar1202@gmail.com>
2024-08-22 22:02:07 +0530

    Test Commit

---
+++ agriculture_system/agriculture_system/doctype/plantation_master/plantation_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Vaibhavi Pawar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Plantation Master', {
+	// refresh: function(frm) {
+
+	// }
+});
